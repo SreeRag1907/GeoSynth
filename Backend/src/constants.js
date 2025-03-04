85cd9e3b50097c7ba8cb54c6ecae67f1c41476b3
Akshay Pise <akshaypise1112@gmail.com>
2025-03-04 20:00:28 +0530

    add Flask server integration and axios for API requests

--- Backend/src/constants.js
+++ Backend/src/constants.js
@@ -1,1 +1,3 @@
 export const DB_NAME = 'myDatabase';
+
+export const FLASK_URL = 'http://localhost:5000';
--- Backend/src/routes/index.route.js
+++ Backend/src/routes/index.route.js
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 import { loginUser, registerUser } from '../controllers/user.controller.js';
+import axios from 'axios';
+import { FLASK_URL } from '../constants.js';
 
 const router = Router();
 
 router.route('/').get((req, res) => {
 	res.json({ message: 'Hello World!' });
+});
+
+router.route('/flask').get(async (req, res) => {
+	try {
+		const response = await axios.get(`${FLASK_URL}/`);
+		res.json(response.data);
+	} catch (error) {
+		res.status(500).json({ error: 'Error connecting to Flask server' });
+	}
 });
 
 router.route('/register').post(registerUser);
